18d597e96067f826bddec681c757e0a170e5f737
Madison Bradley <mlbradle@ualberta.ca>
2022-10-27 17:21:51 -0600

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,7 +13,8 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
     /**
-     * create a mocklist for my citylist * @return
+     * create a mocklist for my citylist
+     * @return 
      */
     public CustomList MockCityList(){
         list = new CustomList(null,new ArrayList<>());
